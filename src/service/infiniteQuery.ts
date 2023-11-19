e7e0b4ba86008e0f46b1f4b8b2a151404395814f
KiX7777 <kristianboi202@gmail.com>
2023-11-19 11:21:20 +0100

    d

--- src/service/infiniteQuery.ts
+++ src/service/infiniteQuery.ts
@@ -6,7 +6,7 @@ export const infiniteQuery = async (page: number) => {
     //   `https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news_desk:World&page=${page}&sort=newest&api-key=6PwsEZoKesC2JSVylxfRq7GOtRHpuxB4`
     // );
 
-    const res = await axios.get(`/infinite?page=${page}`);
+    const res = await axios.get(`/infinite/?page=${page}`);
     if (res.data) return res.data;
   } catch (err) {
     const error = err as Error;
